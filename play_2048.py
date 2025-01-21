eff1f0d86859fa661f097e0ea91d4390ca7ec956
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-01-20 22:04:55 -0500

    bug fixes with game reset

--- play_2048.py
+++ play_2048.py
@@ -2,18 +2,6 @@ from board import Board
 from os import system
 from time import sleep
 
-move_key = {"W":"U",
-            "D":"R",
-            "S":"D",
-            "A":"L"}
-game_key = ["W","D","S","A","R","Q"]
-win_val_options = ["2048", "1024", "512", "256", "128"]
-
-def main():
-    game_start()
-    score = game_loop()
-    game_end(score)
-
 def clear_screen():
     system('clear')
 
@@ -69,6 +57,7 @@ class Game:
         else: 
             self.WIN_VAL = int(win_val)
             self.board = Board(win_val=self.WIN_VAL)
+        self.won = False
 
         clear_screen()
 
@@ -84,7 +73,7 @@ class Game:
             print(f"SCORE: {self.score}\tHIGH SCORE: {self.high_score}")
             print(self.board)
             print("OPTIONS")
-            print("(W) UP\n(D) RIGHT\n(S) DOWN\n(A) LEFT\n(Q) QUIT\n(R) RESTART")
+            print("(W) UP\n(D) RIGHT\n(S) DOWN\n(A) LEFT\n(Q) QUIT\n(R) RESTART\n")
             
             dir = input('Enter your move:\n').upper()
             while dir not in self.GAME_KEY or (dir in self.MOVE_KEY and not self.board.is_valid_move(self.MOVE_KEY[dir])):
@@ -99,17 +88,18 @@ class Game:
             if dir == "R":
                 clear_screen()
                 self.reset()
+                status = self.board.is_end()
                 print("Here is a new board")
                 continue
             
             clear_screen()
-            self.score += self.board.move(move_key[dir])
+            self.score += self.board.move(self.MOVE_KEY[dir])
             self.board.new_tile()
             status = self.board.is_end(self.won)
             if self.score > self.high_score: self.high_score = self.score
         
         if self.BOARD_STATE_KEY[status] == "QUIT":
-            self.end(True)
+            self.end(manual_quit=True)
         else:
             self.end()
 
@@ -119,94 +109,19 @@ class Game:
         """
 
         clear_screen()
+        if not manual_quit: print('GAME OVER')
         print(f'Your high score was {self.high_score}')
         print("Thanks for playing! ")
         if not manual_quit:
-            ans = input("Would you like to play again? y or n").upper()
+            ans = input("Would you like to play again? y or n\n").upper()
             while ans != "Y" and ans != "N":
-                ans = input("Please try again. Would you like to play again? y or n").upper()
+                ans = input("Please try again. Would you like to play again? y or n\n").upper()
             if ans == "Y": 
                 self.reset()
                 self.loop()
             else:
                 clear_screen()
                 print("Thanks for playing!")
-
-def game_start():
-    """introduces game through print statements
-
-    welcomes player into game
-    prompts player to acknowlege by pressing any key
-    clears terminal screen
-    """
-
-    print("Welcome to 2048!")
-    input("To start, press any key\n")
-    clear_screen()
-
-def game_loop() -> int:
-    """main game loop which iteratively requests command or move direction from player, moves tiles, and tracks score
-
-    returns
-    -------
-    int
-        represents the score of the game i.e. the sum of all merged tiles during the game
-    """
-
-    print("What do you want to be the win value?")
-    win_val = input("Please enter one of the following or press enter to use default (2048): [2048, 1024, 512, 256, 128]\n")
-    while win_val != "" and win_val not in win_val_options:
-        win_val = input("Please enter a valid option or press enter to use default (2048): [2048, 1024, 512, 256, 128]\n")
-    if win_val == "": 
-        bd = Board()
-        win_val = 2048
-    else: 
-        win_val = int(win_val)
-        bd = Board(win_val=win_val)
-    high_score = score = 0
-    status = bd.is_end()
-    won = False
-    while status != 2:
-        if status == 1:
-            won = True
-            print(f"Congratulations, you got {win_val}!")
-            input("Press any key to continue\n")
-            clear_screen()
-        print(f"SCORE: {score}")
-        print(bd)
-        print("OPTIONS")
-        print("(W) UP\n(D) RIGHT\n(S) DOWN\n(A) LEFT\n(Q) QUIT\n(R) RESTART")
-        dir = input('Enter your move:\n').upper()
-        while dir not in game_key or (dir in move_key and not bd.is_valid_move(move_key[dir])):
-            if dir not in game_key:
-                print("That is not a valid option. Please refer to the options above.")
-            else:
-                print("Your move did not move any tiles. Please select another direction.")
-            dir = input('Enter a valid move:\n').upper()
-        if dir == "Q":
-            break
-        if dir == "R":
-            clear_screen()
-            bd = Board()
-            if score > high_score: high_score = score
-            score = 0
-            print("Here is a new board")
-            continue
-        clear_screen()
-        score += bd.move(move_key[dir])
-        bd.new_tile()
-        status = bd.is_end(won)
-    if score > high_score: high_score = score
-    return high_score
-
-def game_end(score: int):
-    """displays score of the game and bids adieu to player through print statements
-
-    """
-
-    clear_screen()
-    print(f'Your high score was {score}')
-    print("Thanks for playing!")
 
 if __name__ == "__main__":
     game = Game()
